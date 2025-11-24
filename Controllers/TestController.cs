0fd61bacb2a524391036fe59fa27808bddf65977
gunheehan <rjstmsla@gmail.com>
2025-11-24 23:08:44 +0900

    초기 커밋.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrameworkApp.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestView() 
+        {
+            // return a 'testview.cshtml' view
+            return View();
+        }
+
+        public string Welcome(string Name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {Name}, NumTimes is: {numTimes}");
+        }
+
+        public string Welcome2(string Name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {Name}, ID is: {ID}");
+        }
+
+        public ActionResult ErrorMessage()
+        {
+            return View();
+        }
+
+        public string PrintMessage()
+        {
+            return "<h1>Welcome</h1>";
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrameworkApp.MvcApplication" Language="C#" %>
